5f42c4e3c52694bbf6d8df47e0ee7606ccb9f213
Abhyudai Srivastava <abhyu0011@gmail.com>
2023-09-12 19:44:04 +0530

    Third Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-
 import './App.css';
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Navbar from './Components/Navbar';
+import { Home } from './Components/Home';
+import About from './Components/About';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNotebook</h1>
-    </div>
+    <>
+    <Router>
+        <Navbar/>
+        
+        <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route exact path="/about" element={<About/>}/>
+            
+        </Routes>
+    </Router>
+    </>
   );
 }
 
---
+++ src/Context/notes/noteContext.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const noteContext = createContext(); //to create context this is the syntax. Context will hold all the states of the notes
+
+export default noteContext;
